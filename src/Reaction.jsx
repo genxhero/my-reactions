bc62819a1ba84915b9a0c7316a206aacbf4b19a6
genxhero <genxhero@users.noreply.github.com>
2020-03-10 20:18:21 -0700

    import reactioon into reaction section

--- src/Reaction.jsx
+++ src/Reaction.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import * as ICONS from 'react-icons';
 
 const Reaction = props => {
+    debugger;
     return (
         <div>
 
         </div>
     )
 }
+
+// Reaction.defaultProps = {
+//     icon: ICONS[]
+// }
 
 export default Reaction;
--- src/ReactionSection.jsx
+++ src/ReactionSection.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Reaction from './Reaction';
 
 const ReactionSection = props => {
     return (
         <div>
-
+            <Reaction />
         </div>
     )
 }
