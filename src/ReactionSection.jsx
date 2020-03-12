68ab433d2d002db0a40fcbc29d048afc53b9b932
genxhero <genxhero@users.noreply.github.com>
2020-03-11 23:02:21 -0700

    add more proptypes to reactionsection

--- src/ReactionSection.jsx
+++ src/ReactionSection.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import Reaction from './Reaction';
+import PropTypes from 'prop-types';
+import * as FA from 'react-icons/fa';
+import * as MD from "react-icons/md";
+
+const LIBRARIES = {"FA": FA, "MD": MD}
 
 const ReactionSection = props => {
     return (
@@ -8,5 +13,9 @@ const ReactionSection = props => {
         </div>
     )
 }
+
+    ReactionSection.defaultProps = {
+        library: "FA"
+    }
 
 export default ReactionSection;
